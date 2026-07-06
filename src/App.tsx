8a9768aaffbafaf5e3093f6322188a5e02aa42a4
rayyansaqib12-byte <rayyansaqib12@gmail.com>
2026-07-06 23:30:47 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {Home} from './components/Home';
+import {Footer} from './components/Footer';
 import {AnimatedBackground} from './components/AnimatedBackground';
 
 export default function App() {
